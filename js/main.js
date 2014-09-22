0acbff8b9a82ae3fffb5e3f1ca71749384a11bbc
routerfrank <ayfrank2@gmail.com>
2014-09-22 07:57:21 +0100

    init repo

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var portfolioAnimate = {
+
+
+ smoothScroll: function() {
+
+    $('a[href^=#]').on('click', function(event) {
+      event.preventDefault();
+      $('html, body').animate({ scrollTop: $( $(this).attr('href') ).offset().top }, 1000);
+    });
+	
+  },
+
+	/*textAnimate: function() {
+		var divWidth = $("#logo").width() / 100 * 10;
+		$('h2').animate({'marginLeft': divWidth + "px"}, 4000);
+		$('h1').animate({'marginRight': divWidth + "px"}, 2000);
+		},*/
+
+	
+	initialize: function() {
+		/*portfolioAnimate.textAnimate();*/
+		portfolioAnimate.smoothScroll();
+	}
+
+}
+
+$(document).ready(portfolioAnimate.initialize);
